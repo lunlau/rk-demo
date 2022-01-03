97d0a36fc6b19617bd419dd012d10b29d8193d65
andyklliu <>
2022-01-03 13:28:26 +0800

    添加上报函数

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module github.com/lunlau/rk-demo
 go 1.16
 
 require (
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.1 // indirect
-	github.com/rookie-ninja/rk-boot v1.4.0
-	github.com/rookie-ninja/rk-boot/grpc v1.2.15
-	google.golang.org/grpc v1.42.0
-	google.golang.org/protobuf v1.27.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/prometheus/client_golang v1.10.0
+	github.com/prometheus/common v0.22.0 // indirect
+	github.com/shirou/gopsutil v2.21.11+incompatible
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20210608053332-aa57babbf139 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
--- main.go
+++ main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
-	"context"
-	rkboot "github.com/rookie-ninja/rk-boot"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/shirou/gopsutil/mem"
 )
 
-// Application entrance.
 func main() {
-	// Create a new boot instance.
-	boot := rkboot.NewBoot()
+	//初始化日志服务
+	logger := log.New(os.Stdout, "[Memory]", log.Lshortfile|log.Ldate|log.Ltime)
 
-	// Bootstrap
-	boot.Bootstrap(context.Background())
+	//初始一个http handler
+	http.Handle("/metrics", promhttp.Handler())
 
-	// Wait for shutdown sig
-	boot.WaitForShutdownSig(context.Background())
+	//初始化一个容器
+	diskPercent := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "memeory_percent",
+		Help: "memeory use percent",
+	},
+		[]string{"percent"},
+	)
+	prometheus.MustRegister(diskPercent)
+
+	// 启动web服务，监听8080端口
+	go func() {
+		logger.Println("ListenAndServe at:0.0.0.0:8080")
+		err := http.ListenAndServe("127.0.0.1:8080", nil)
+
+		if err != nil {
+			logger.Fatal("ListenAndServe: ", err)
+		}
+	}()
+
+	//收集内存使用的百分比
+	for {
+		logger.Println("start collect memory used percent!")
+		v, err := mem.VirtualMemory()
+		if err != nil {
+			logger.Println("get memeory use percent error:%s", err)
+		}
+		usedPercent := v.UsedPercent
+		logger.Println("get memeory use percent:", usedPercent)
+		diskPercent.WithLabelValues("usedMemory").Set(usedPercent)
+		time.Sleep(time.Second * 2)
+	}
 }
